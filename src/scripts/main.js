03023cd04c3b8bee860857915db188e2681deb3f
Fedos17 <dfkd81@gmail.com>
2024-05-02 10:34:06 +0300

    fix overflow menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const body = document.querySelector('.page');
+
+window.onhashchange = (e) => {
+  const menu = e.newURL.includes('#menu')
+    ? body.classList.add('page-overflow')
+    : body.classList.remove('page-overflow');
+
+  return menu;
+};
